9667e927af01109d3c60674f51e7328542f062d7
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-09-13 15:03:46 +0500

    Task7

---
+++ Tyuiu.NikitinRYu.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NikitinRYu.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            var nums1 = new int[] { 1, 1, 1, 1, 1 };
+            var nums2 = new int[] { 1, 1, 1, 1, 1 };
+            var nums3 = new int[] { 2, 2, 2, 2, 2 };
+
+            var res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+        }
+    }
+}
---
+++ Tyuiu.NikitinRYu.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NikitinRYu.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NikitinRYu.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0be4a81e-e63d-4862-b698-11653b94c7de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
